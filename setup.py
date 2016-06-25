1df7661cca85355b266d2d6b66948ec7347c82ba
jamesstidard <jamesstidard@gmail.com>
2016-06-25 16:11:01 +0100

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Talk Zoho',
-    version='0.1.dev0',
+    version='0.1.dev1',
     packages=['talkzoho', 'talkzoho.crm'],
     long_description=open('README.md').read(),
 )
