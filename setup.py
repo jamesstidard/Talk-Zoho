3bf0eb41596ab8adc1b113e5604d6b0c723194c2
jamesstidard <jamesstidard@gmail.com>
2016-06-27 13:43:54 +0100

    add requirements to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from distutils.core import setup
+from pip.req import parse_requirements
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
 
 setup(
     name='Talk Zoho',
@@ -9,4 +19,5 @@ setup(
         'talkzoho.books'
     ],
     long_description=open('README.md').read(),
+    install_requires=reqs,
 )
