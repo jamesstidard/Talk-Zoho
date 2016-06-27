b1a916610b8f7eb900ce7a1f73b31faca8482efa
jamesstidard <jamesstidard@gmail.com>
2016-06-27 19:19:29 +0100

    clean

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 from distutils.core import setup
-from pip.req import parse_requirements
-
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt', session=False)
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
+from pip.req import parse_requirements as parse
 
 
 setup(
@@ -19,5 +11,5 @@ setup(
         'talkzoho.books'
     ],
     long_description=open('README.md').read(),
-    install_requires=reqs,
+    install_requires=[str(r.req) for r in parse('requirements.txt', session=False)],  # noqa
 )
