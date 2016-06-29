91c6d2ff5a83ee011cc7dee4fae5588e1148439c
jamesstidard <jamesstidard@gmail.com>
2016-06-29 15:34:00 +0100

    bump

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 from pip.req import parse_requirements as parse
 
 
 setup(
     name='Talk Zoho',
-    version='0.2.dev1',
-    packages=['talkzoho'],
+    version='0.2.dev2',
+    packages=find_packages(exclude=('tests', 'tests.*'),
     long_description=open('README.md').read(),
     install_requires=[str(r.req) for r in parse('requirements.txt', session=False)],  # noqa
 )
