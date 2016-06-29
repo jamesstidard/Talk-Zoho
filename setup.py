312f15004e100356c9b5c6f7f4b3381016d9e3fb
jamesstidard <jamesstidard@gmail.com>
2016-06-29 15:30:36 +0100

    bump

--- setup.py
+++ setup.py
@@ -4,12 +4,8 @@ from pip.req import parse_requirements as parse
 
 setup(
     name='Talk Zoho',
-    version='0.2.dev0',
-    packages=[
-        'talkzoho',
-        'talkzoho.crm',
-        'talkzoho.books'
-    ],
+    version='0.2.dev1',
+    packages=['talkzoho'],
     long_description=open('README.md').read(),
     install_requires=[str(r.req) for r in parse('requirements.txt', session=False)],  # noqa
 )
