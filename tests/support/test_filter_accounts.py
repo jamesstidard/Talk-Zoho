cb3f114421a955e927a07d6afc78a43dbcdbb0e7
jamesstidard <jamesstidard@gmail.com>
2016-07-12 11:40:39 +0100

    init test files for support

---
+++ tests/support/test_filter_accounts.py
@@ -0,0 +1,66 @@
+import pytest
+
+from tornado.web import HTTPError
+
+from tests.projects.fixtures import *  # noqa
+
+from talkzoho.regions import EU
+from talkzoho.support import filter_accounts
+
+
+@pytest.mark.gen_test
+def test_one_account_is_list(auth_token):
+    accounts = yield filter_accounts(
+        auth_token=auth_token,
+        region=EU,
+        limit=1)
+    assert len(accounts) == 1
+    assert isinstance(accounts, list)
+
+
+# @pytest.mark.gen_test
+# def test_account_looks_right(auth_token):
+#     accounts = yield filter_accounts(auth_token=auth_token, region=EU, limit=1)
+#     account  = accounts[0]
+#     assert 'ACCOUNTID' in account.keys()
+#     assert len(account.keys()) > 5
+#
+#
+# @pytest.mark.gen_test
+# def test_column_whitelist(auth_token):
+#     accounts = yield filter_accounts(
+#         auth_token=auth_token,
+#         region=EU,
+#         limit=1,
+#         columns=['ACCOUNTID', 'Account Name'])
+#     account = accounts[0]
+#     assert 'ACCOUNTID' in account.keys()
+#     assert 'Account Name' in account.keys()
+#     assert len(account.keys()) == 2
+#
+#
+# @pytest.mark.gen_test
+# def test_search_term(auth_token):
+#     accounts = yield filter_accounts(
+#         auth_token=auth_token,
+#         region=EU,
+#         limit=1,
+#         term='IFP')
+#     account = accounts[0]
+#     assert account['Account Name'] == 'IFPL'
+#
+#
+# @pytest.mark.gen_test
+# def test_zero_limit(auth_token):
+#     accounts = yield filter_accounts(
+#         auth_token=auth_token,
+#         region=EU,
+#         limit=0)
+#     assert len(accounts) == 0
+#
+#
+# @pytest.mark.gen_test
+# def test_404(auth_token):
+#     with pytest.raises(HTTPError) as excinfo:
+#         yield filter_accounts(auth_token=auth_token, region=EU, offset=40000)
+#     assert excinfo.value.status_code == 404
