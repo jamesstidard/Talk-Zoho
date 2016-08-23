45b03842455bbc189f8746000de1b338220bf464
jamesstidard <jamesstidard@gmail.com>
2016-08-23 09:59:27 +0100

    include get / filter crm contacts in init

--- talkzoho/crm/__init__.py
+++ talkzoho/crm/__init__.py
@@ -5,10 +5,12 @@ SCOPE               = 'crmapi'
 MAX_PAGE_SIZE       = 200
 REQUESTS_PER_SECOND = None
 
+from .contacts.get_contact import get_contact  # noqa
 from .accounts.get_account import get_account  # noqa
 from .leads.get_lead import get_lead  # noqa
 from .get_module import get_module  # noqa
 
+from .contacts.filter_contacts import filter_contacts  # noqa
 from .accounts.filter_accounts import filter_accounts  # noqa
 from .leads.filter_leads import filter_leads  # noqa
 from .filter_module import filter_module  # noqa
