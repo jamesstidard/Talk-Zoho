c5951c31fb871e87c644c19704ffde8ee5592b6d
jamesstidard <jamesstidard@gmail.com>
2016-08-31 20:39:18 +0100

    package name and git url

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from pip.req import parse_requirements as parse
 
 
 setup(
-    name='Talk Zoho',
+    name='talkzoho',
     version='0.4.dev2',
+    url='https://github.com/A2Z-Cloud/Talk-Zoho',
     packages=find_packages(exclude=('tests', 'tests.*')),
     long_description=open('README.md').read(),
     install_requires=[str(r.req) for r in parse('requirements/production.txt', session=False)],  # noqa
