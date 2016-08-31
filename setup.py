4c37646b54762148fd1e1d62adfc947bc7f0f374
jamesstidard <jamesstidard@gmail.com>
2016-08-31 21:57:15 +0100

    decaps

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name='talkzoho',
     version='0.5.9',
-    description='Asynchronous Wrapper for Zoho\'s Numerous APIs',
+    description='Asynchronous wrapper for Zoho\'s numerous APIs',
     url='https://github.com/A2Z-Cloud/Talk-Zoho',
     packages=find_packages(exclude=('tests', 'tests.*')),
     install_requires=[
