33e8c6760be362a71ffd192545707e9657d6afc3
jamesstidard <jamesstidard@gmail.com>
2016-09-30 10:44:01 +0100

    wait test

--- tests/crm/test_get_account.py
+++ tests/crm/test_get_account.py
@@ -3,11 +3,20 @@ import pytest
 from tests.crm.fixtures import *  # noqa
 
 from talkzoho.crm import get_account
+from talkzoho.utils import wait
 
 
 @pytest.mark.gen_test
 def test_can_get_account(auth_token, account_id):
     account = yield get_account(
+        auth_token=auth_token,
+        id=account_id)
+    assert account['ACCOUNTID'] == account_id
+
+
+def test_can_get_account_sync(auth_token, account_id):
+    account = wait(
+        get_account,
         auth_token=auth_token,
         id=account_id)
     assert account['ACCOUNTID'] == account_id
