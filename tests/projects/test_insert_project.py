e21397dd5de56580ec916eb731b6d2cf006f3fe0
jamesstidard <jamesstidard@gmail.com>
2016-11-29 15:17:57 +0000

    insert/update project

--- tests/projects/test_insert_project.py
+++ tests/projects/test_insert_project.py
@@ -4,8 +4,8 @@ from tests.projects.fixtures import *  # noqa
 
 
 @pytest.mark.gen_test
-def test_can_get_project(projects, portal_id):
-    project_id = yield projects.projects.insert(
-        {'name': 'Test Project'},
-        portal_id=portal_id)
+def test_can_insert_project(projects, portal_id):
+    project_id = yield projects.projects.insert({
+        'name': 'Test Project',
+        'portal_id': portal_id})
     assert project_id
--- tests/projects/test_update_project.py
+++ tests/projects/test_update_project.py
@@ -5,14 +5,14 @@ from tests.projects.fixtures import *  # noqa
 
 @pytest.mark.gen_test
 def test_can_get_project(projects, portal_id):
-    project_id = yield projects.projects.insert(
-        {'name': 'Inserted Project'},
-        portal_id=portal_id)
-    updated_project = yield projects.projects.update(
-        {'id': project_id,
+    project_id = yield projects.projects.insert({
+        'name': 'Inserted Project',
+        'portal_id': portal_id})
+    updated_project = yield projects.projects.update({
+        'id': project_id,
         'name': 'Updated Name',
-        'status': 'active'},
-        portal_id=portal_id)
+        'status': 'active',
+        'portal_id': portal_id})
     assert updated_project['id'] == project_id
     assert updated_project['name'] == 'Updated Name'
     yield projects.projects.delete(project_id, portal_id=portal_id)
